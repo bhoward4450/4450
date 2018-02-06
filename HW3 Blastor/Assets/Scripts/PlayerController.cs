deb5a1de9f732c2bb93889665ea25882df39f17f
bhoward4450 <bradleyhoward@mail.weber.edu>
2018-02-05 22:15:49 -0700

    Menu completed, PlayArea started
    Added text and button to Menu
    Created prefabs for Good/Bad Pickup objects
    Created player object
    Added background

---
+++ HW3 Blastor/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	//moves the player character
+
+	//Vars for the boundary of the play area. Determines movement distances
+	public int boundX;
+	public int boundY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
