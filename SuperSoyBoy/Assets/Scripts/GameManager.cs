a83690f17fdc4e4cec9262dae5f2cab8f3995547
bhoward4450 <bradleyhoward@mail.weber.edu>
2018-02-13 21:36:16 -0700

    Added multiple scripts
    Up to 62

--- SuperSoyBoy/Assets/Scripts/GameManager.cs
+++ SuperSoyBoy/Assets/Scripts/GameManager.cs
@@ -2,27 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
+	public string playerName;
 
-	void Awake(){
-		if (instance == null){
+	void Awake() {
+		if(instance == null) {
 			instance = this;
-		} else if (instance != this){
-			Destroy(gameObject);
-		}
+		} else if(instance != this) {
+				Destroy(gameObject);
+			}
 		DontDestroyOnLoad(gameObject);
 	}
 
-	public void RestartLevel(float delay){
+	public void RestartLevel(float delay) {
 		StartCoroutine(RestartLevelDelay(delay));
 	}
 
-	private IEnumerator RestartLevelDelay(float delay){
+	private IEnumerator RestartLevelDelay(float delay) {
 		yield return new WaitForSeconds(delay);
 		SceneManager.LoadScene("LevelSoy");
+	}
+
+	public List<PlayerTimeEntry> LoadPreviousTimes() { 
+		// 1 
+		try { 
+			var scoresFile = Application.persistentDataPath +
+			                 "/" + playerName +
+			                 "_times.dat"; 
+			using(var stream = File.Open(scoresFile, FileMode.Open)) {
+				var bin = new BinaryFormatter();
+				var times = (List<PlayerTimeEntry>)bin.Deserialize(stream);
+				return times; 
+			} 
+		} 
+		// 2 
+		catch(IOException ex) {
+			Debug.LogWarning("Couldn’t load previous times for: "
+			+
+			playerName +
+			". Exception: "
+			+ ex.Message);
+			return new List<PlayerTimeEntry>();
+		} 
+	}
+
+	public void SaveTime(decimal time) { 
+		// 3 
+		var times = LoadPreviousTimes();
+		// 4 
+		var newTime = 
+			new PlayerTimeEntry();
+		newTime.entryDate = DateTime.Now;
+		newTime.time = time;
+		// 5 
+		var bFormatter = 
+			new BinaryFormatter();
+		var filePath = Application.persistentDataPath +
+		               "/" + playerName +
+		               "_times.dat"; 
+		using(var file = File.Open(filePath, FileMode.Create)) {
+			times.Add(newTime);
+			bFormatter.Serialize(file, times);
+		} 
 	}
 
 }
